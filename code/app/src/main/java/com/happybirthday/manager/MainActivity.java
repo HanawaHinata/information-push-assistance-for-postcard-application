11d8f4099bff8801b6afd314b904183f11c6c9f4
Hinata Hanawa <bainesing@nebula-soft.com>
2018-04-01 14:04:32 +0800

    更新注释

--- code/app/src/main/java/com/happybirthday/manager/MainActivity.java
+++ code/app/src/main/java/com/happybirthday/manager/MainActivity.java
@@ -7,18 +7,13 @@ import android.app.AlertDialog.Builder;
 import android.os.Bundle;
 import android.os.Handler;
 public class MainActivity extends Activity {
-	private final int SPLASH_DISPLAY_LENGHT = 5000; //��λΪ���룬5000�ʹ��5�롣
+	private final int SPLASH_DISPLAY_LENGHT = 5000; //延时5秒
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		new Handler().postDelayed(new Runnable() {
 				public void run(){
-					
-					
-					
-					
-					
 					Intent mainIntent = new Intent(MainActivity.this,downloadact.class);
 					MainActivity.this.startActivity(mainIntent);
 					MainActivity.this.finish();
